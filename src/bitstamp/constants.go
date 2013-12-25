e2acad166f017b280466959743afedf5c3e27ada
David de Kloet <dskloet@gmail.com>
2013-12-25 13:50:34 +0100

    Forgot the bitstamp package

---
+++ src/bitstamp/constants.go
@@ -0,0 +1,17 @@
+package bitstamp
+
+const (
+    API_URL = "https://www.bitstamp.net/api/"
+    API_BALANCE = "balance/"
+    API_OPEN_ORDERS = "open_orders/"
+    API_CANCEL_ORDER = "cancel_order/"
+    API_BUY = "buy/"
+    API_SELL = "sell/"
+)
+
+const (
+    USD_BALANCE = "usd_balance"
+    BTC_BALANCE = "btc_balance"
+    FEE = "fee"
+)
+
