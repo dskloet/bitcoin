e10385107f4c3c58afd6e38cf2c6a13069e2a9f6
David de Kloet <dskloet@gmail.com>
2014-09-28 14:59:14 +0200

    Kraken: pay fee in BTC when selling.

--- src/bitcoin/kraken/addorder.go
+++ src/bitcoin/kraken/addorder.go
@@ -19,13 +19,15 @@ func (client *Client) Sell(price, amount float64) (err error) {
 }
 
 func (client *Client) addOrder(buySell string, price, amount float64) (err error) {
-
   params := client.createParams()
   params.Set("pair", client.currencyPair)
   params.Set("type", buySell)
   params.Set("ordertype", "limit")
   params.Set("price", fmt.Sprintf("%.5f", price))
   params.Set("volume", fmt.Sprintf("%.5f", amount))
+  if buySell == "sell" {
+    params.Set("oflags", "plbc")
+  }
   if client.dryRun {
     params.Set("validate", "true")
   }
